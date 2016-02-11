3fbed6cc9c91d876f5d9ebf770c17b80b9c6a22c
paulopez78 <pau.lopez.78@gmail.com>
2016-02-11 11:57:25 +0100

    dockerfile with bower

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 cd ./Voting.Admin
-dnu restore
-dnu publish
 docker build -t paulopez/voting-admin:latest .
 docker push paulopez/voting-admin
 
 cd ../Voting.Api
-dnu restore
-dnu publish
 docker build -t paulopez/voting-api:latest .
 docker push paulopez/voting-api
 
--- start.sh
+++ start.sh
@@ -7,10 +7,6 @@ cd ../Voting.Api
 dnu restore
 dnu publish
 
-cd ../voting-ui
-npm update
-bower update
-
 cd ..
 docker-compose build
 docker-compose up
